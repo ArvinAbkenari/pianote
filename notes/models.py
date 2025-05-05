31d808fe742e0da281fa32e895376a5a72c87dec
shayanfattahi <109668751+shayanfattahi@users.noreply.github.com>
2025-05-05 18:18:04 +0330

    Add Note model with fields and migration; configure media settings

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from bson import ObjectId
 
-# Create your models here.
+User = get_user_model()
+
+class Note(models.Model):
+    id = models.CharField(max_length=24, primary_key=True, editable=False)
+    name = models.CharField(max_length=150)
+    genre = models.JSONField()
+    description = models.TextField(blank= True, null=True)
+    level = models.IntegerField()
+    rate = models.IntegerField()
+    createdAt = models.DateTimeField()
+    deleteFlag = models.BooleanField(default=False)
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.JSONField(default=list)
+    notesheet = models.FileField(upload_to='notesheets/', null=True, blank=True)
+    audio = models.FileField(upload_to='audio/', null=True, blank=True)
+    def add_comment(self, user_id, text, createdDate , delete_flag=False):
+        comment = {
+            "userID": user_id,
+            "text": text,
+            "createdAt": createdDate,
+            "deleteFlag": delete_flag
+        }
+        self.comments.append(comment)
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+@property
+def is_authenticated(self):
+    return True
